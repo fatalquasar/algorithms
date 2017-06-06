833becfb263e7c5f76094e891d8cd11fccd79fbf
fatalquasar <cuigh80@163.com>
2017-06-06 23:14:00 +0800

    Add files via upload

---
+++ 1.1E.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 06/06/2017 21:48
+
+#(欧几里得算法) 给定两个正整数m和n，求他们的最大公因数，即同时整除m和n的最大正整数。
+#E1. 【求余数】用n除m，令r为余数。（我们将有0<=r<n）
+#E2. 【余数为0？】如果r=0，算法终止。n为答案。
+#E3. 【减少】置m<-n,n<-r,然后返回步骤E1
+
+
+def greatest_common_factor(n, m):
+    if n > m:   # 如果n>m, 互换参数位置
+        return greatest_common_factor(m, n)
+    if m % n == 0:
+        return n
+    else:
+        return greatest_common_factor(m % n, n)
